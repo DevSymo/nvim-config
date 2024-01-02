4e3304ce64d6f768beed3de5d26f7b4530305952
DevSymo <118448408+DevSymo@users.noreply.github.com>
2024-01-02 08:44:16 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -3,8 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-vim.api.nvim_set_keymap('n', '<C-Tab>', ':BufferNext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-S-Tab>', ':BufferPrevious<CR>', { noremap = true, silent = true })
 vim.wo.number = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
